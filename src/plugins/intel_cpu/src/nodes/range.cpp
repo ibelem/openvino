1e8952584c093c83ec5de22ec29aca63de85cc29
Belem Zhang <belem.zhang@intel.com>
2026-06-25 07:49:34 +0800

    add src/plugins/intel_cpu/src/nodes/range.cpp (security finding)

--- src/plugins/intel_cpu/src/nodes/range.cpp
+++ src/plugins/intel_cpu/src/nodes/range.cpp
@@ -140,6 +140,9 @@ size_t Range::getWorkAmount(data_t* startPtr, data_t* stopPtr, data_t* stepPtr) 
     if (std::is_same_v<data_t, int>) {
         auto iSpan = static_cast<int>(span);
         auto iStep = static_cast<int>(step);
+        if (iStep == 0) {
+            OPENVINO_THROW("Range: step must be non-zero");
+        }
         return static_cast<size_t>(div_up(iSpan < 0 ? -iSpan : iSpan, iStep < 0 ? -iStep : iStep));
     }
     return static_cast<size_t>(std::ceil(std::fabs(span) / std::fabs(step)));
